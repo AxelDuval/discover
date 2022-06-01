2bd97b997ddd49a34a8bc2aea9fad4efc0be32e2
axel@77 <axel@bearstudio.fr>
2022-06-01 09:26:26 +0200

    logs users

--- pages/users/index.js
+++ pages/users/index.js
@@ -12,6 +12,8 @@ export async function getStaticProps() {
 
   const response = await fetch(`https://placesdiscover.herokuapp.com/api/users`);
   const users = await response.json();
+  console.log(response);
+  console.log(users);
   return {
     props: {
       users,
